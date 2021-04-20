cca032f5535a02ad2e0f906d981a5492a1489f55
Andrea <andreasin88@gmail.com>
2021-04-20 19:39:38 +0200

    reducer example

--- src/main/java/DriverReduction.java
+++ src/main/java/DriverReduction.java
@@ -1,11 +1,18 @@
+import java.util.function.BinaryOperator;
 import java.util.stream.Stream;
 
 public class DriverReduction {
 
 	public static void main(String[] args) {
+		
+		// ___REDUCTION___
+		// reduction porta ad un solo elemento il contenuto, attraverso una funzione
 
 		Stream<Integer> stream = Stream.of(1,2,3);
-		BinaryOperation<Integer> sum = (i1, i2) -> i1 + i2;
+		BinaryOperator<Integer> sum = (i1, i2) -> i1 + i2;
+		Integer id = 99; // se il risultato fosse 0 usa l'identity
+		int red = stream.reduce(id, sum);
+		System.out.println(red);
 		
 		
 		
